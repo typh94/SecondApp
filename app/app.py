24c28d7b5696d4a5f797fb8b7af17cf158be94a6
Typhene Sonia Benfriha <typh94@live.fr>
2024-06-12 20:43:21 -0500

    added background color and adjusted nav

--- app/app.py
+++ app/app.py
@@ -14,7 +14,7 @@ def about():
 
 @app.route('/contact')
 def contact():
-    return 'This is the Contact page.'
+    return render_template('main/contact.html')
 
 if __name__ == '__main__':
 #  app.run(host='127.0.0.1', port=5000)
